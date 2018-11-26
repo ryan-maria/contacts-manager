3b863132341e7367f1395d35538d39dfd58ca1e3
Ryan-Houpt <rehoupt@gmail.com>
2018-11-26 14:39:03 -0600

    added method names

---
+++ src/ContactsApp.java
@@ -0,0 +1,25 @@
+public class ContactsApp {
+
+
+    public static void View() {
+
+    }
+
+    public static void Add() {
+
+    }
+
+    public static void Search() {
+
+    }
+
+    public static void Delete() {
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
